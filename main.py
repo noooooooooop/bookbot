42dc80fd356638966d1bab9e08e3afe4c6972cfd
nooooooop <4056698+noooooooooop@users.noreply.github.com>
2024-05-16 14:51:12 -0400

    Add count_letters

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import re
+
+def count_letters(path):
+    letter_counts = {}
+
+    with open(path) as f:
+        file_contents = f.read()
+
+        filtered = re.sub(r"[^a-zA-Z]", "", file_contents)
+
+        for char in filtered:
+            if not char.lower() in letter_counts:
+                letter_counts[char.lower()] = 0
+
+            letter_counts[char.lower()] += 1
+
+        print(letter_counts)
+
 def count_chars(path):
     with open(path) as f:
         file_contents = f.read()
@@ -11,3 +29,4 @@ def count_words(path):
 if __name__ == "__main__":
     count_words("books/frankenstein.txt")
     count_chars("books/frankenstein.txt")
+    count_letters("books/frankenstein.txt")
