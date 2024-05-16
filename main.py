ce8230ea7e88bd61a2c801f0a8c532904a863620
nooooooop <4056698+noooooooooop@users.noreply.github.com>
2024-05-16 15:12:11 -0400

    Create a simple frequency report

--- main.py
+++ main.py
@@ -14,19 +14,36 @@ def count_letters(path):
 
             letter_counts[char.lower()] += 1
 
-        print(letter_counts)
+        items = list((x, y) for (y, x) in letter_counts.items())
+        items.sort(reverse=True)
+
+        report = list(f"The '{letter}' character was found {count} times" for (count, letter) in items)
+
+        return report
 
 def count_chars(path):
     with open(path) as f:
         file_contents = f.read()
-        print(len(file_contents))
+
+        return len(file_contents)
 
 def count_words(path):
     with open(path) as f:
         file_contents = f.read()
-        print(len(file_contents.split()))
+
+        return len(file_contents.split())
 
 if __name__ == "__main__":
-    count_words("books/frankenstein.txt")
-    count_chars("books/frankenstein.txt")
-    count_letters("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+
+    words = count_words(path)
+    chars = count_chars(path)
+    letter_counts = count_letters(path)
+
+    print(f"--- Begin report of {path} ---")
+    print(f"{words} words found in the document")
+
+    print("")
+
+    for i in range(0, len(letter_counts)):
+        print(letter_counts[i])
