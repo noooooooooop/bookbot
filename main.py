84315d00b34df149999b09090822c1a20300839b
nooooooop <4056698+noooooooooop@users.noreply.github.com>
2024-05-16 14:39:45 -0400

    Adding count chars

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+def count_chars(path):
+    with open(path) as f:
+        file_contents = f.read()
+        print(len(file_contents))
+
 def count_words(path):
     with open(path) as f:
         file_contents = f.read()
@@ -5,3 +10,4 @@ def count_words(path):
 
 if __name__ == "__main__":
     count_words("books/frankenstein.txt")
+    count_chars("books/frankenstein.txt")
